00034fd9d86f4269f3a81d9f79b324237e35b67c
Ezekiel A. Mitchell <105891298+ezekielamitchell@users.noreply.github.com>
2022-09-24 20:43:24 -0700

    hands vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// use std::thread::sleep;
+
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    greeting();
+}
+
+fn greeting() {
+    println!("--- Rock-Paper-Scissors ---");
+    game();
+}
+
+fn game() {
+    let hands = vec!["rock", "paper", "scissors"];
+    for hand in hands {
+        println!("{}", hand);
+    }
 }
