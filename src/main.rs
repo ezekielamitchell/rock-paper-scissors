9c2dd59790594f50fe4c644630ecd2d418df0106
Ezekiel A. Mitchell <105891298+ezekielamitchell@users.noreply.github.com>
2022-09-25 22:22:01 -0700

    1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-// use std::thread::sleep;
-
 #[allow(unused_variables)]
+#[allow(unused_imports)]
+
+// use std::io;
+// use std::io::prelude::*;
+// use std::fs::File;
+use rand::Rng;
 
 fn main() {
     greeting();
+    game();
 }
 
 fn greeting() {
     println!("--- Rock-Paper-Scissors ---");
-    game();
 }
 
 fn game() {
+
+    // generate computer hand
+    let mut rng = rand::thread_rng();
+    let random_number = rng.gen_range(0..3);
+
     let hands = vec!["rock", "paper", "scissors"];
-    for hand in hands {
-        println!("{}", hand);
+    let hand = hands[random_number];
+
+    println!("current hand: {}", hand);
+
+    // request user hand
+    println!("Choose your hand:");
+
+    let mut number = 1;
+    for h in hands {
+        println!("{} : {}", number, h);
+        number += 1;
     }
+
+
 }
